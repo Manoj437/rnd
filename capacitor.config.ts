f6b7fc99176d23e59fe3bb54dc7cb97575a37cee
manoj437 <manoj437@gmail.com>
2023-06-28 17:16:26 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'indian_car_rental',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
